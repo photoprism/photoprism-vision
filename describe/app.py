aefc24fcd359bab40a0f16e0a6f527bcafc55909
Michael Mayer <michael@photoprism.app>
2024-07-27 10:02:36 +0200

    Describe: Update service to listen on 0.0.0.0:5000

--- describe/app.py
+++ describe/app.py
@@ -153,4 +153,4 @@ def blipGenerateResponse():
     return jsonify({"processed_text": processed_text}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)

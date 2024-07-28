5a361f184c90ce16df53a22e1b69f28d354033ff
Michael Mayer <michael@photoprism.app>
2024-07-28 11:50:00 +0200

    CI: Refactor requirements.sh script

--- describe/scripts/requirements.sh
+++ describe/scripts/requirements.sh
@@ -8,29 +8,12 @@ fi
 
 set -o errexit
 
-SYSTEM_ARCH=$(uname -m)
-DESTARCH=${BUILD_ARCH:-$SYSTEM_ARCH}
+# Create virtual environment.
+python3 -m venv ./venv
+. ./venv/bin/activate
 
-case $DESTARCH in
-  amd64 | AMD64 | x86_64 | x86-64)
-    export HDF5_LIBDIR="/usr/lib/x86_64-linux-gnu/hdf5"
-    ;;
+# Upgrade pip package manager.
+./venv/bin/pip install --disable-pip-version-check --no-cache-dir --upgrade pip
 
-  arm64 | ARM64 | aarch64)
-    export HDF5_LIBDIR="/usr/lib/aarch64-linux-gnu/hdf5"
-    ;;
-
-  arm | ARM | aarch | armv7l | armhf)
-    export HDF5_LIBDIR="/usr/lib/arm-linux-gnueabihf/hdf5"
-    ;;
-
-  *)
-    echo "Unsupported Machine Architecture: \"$DESTARCH\"" 1>&2
-    exit 1
-    ;;
-esac
-
-python3 -m venv /app/venv
-. /app/venv/bin/activate
-/app/venv/bin/pip install --disable-pip-version-check --no-cache-dir --upgrade pip
-/app/venv/bin/pip install --disable-pip-version-check --no-cache-dir -r requirements.txt
+# Install Python dependencies.
+./venv/bin/pip install --disable-pip-version-check --no-cache-dir -r requirements.txt

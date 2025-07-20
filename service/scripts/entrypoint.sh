0927d068e157b4656e87a04fb75196eea0dd444f
Lukáš Kvídera <kvideral@qwsome.eu>
2025-07-20 17:26:07 +0200

    #16 Fixed gosu call if container is started as defined user
    - f.e. if compose.yml contains user: 1000:1000, it is not possible to switch user
    
    - we can switch only from root user

--- service/scripts/entrypoint.sh
+++ service/scripts/entrypoint.sh
@@ -4,11 +4,11 @@
 
 . ./venv/bin/activate
 
-if [ ! -z "$PHOTOPRISM_UID" ]; then
-  echo "Switching to user id $PHOTOPRISM_UID..."
+if [[ ! -z "$PHOTOPRISM_UID" && $(id -u) -eq 0 ]]; then
+  echo "Switching from $(whoami) ($(id -u)) to user id $PHOTOPRISM_UID..."
   exec gosu $PHOTOPRISM_UID gunicorn "$@"
 else
-  # Run as default user
+  echo "Running as default user $(whoami) ($(id -u))..."
   exec gunicorn "$@"
 fi
 
